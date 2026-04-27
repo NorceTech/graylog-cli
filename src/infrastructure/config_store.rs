123a6414768727535117883bbdbbe20cc6956dd0
Jakob Ankarhem <jakob@ankarhem.dev>
2026-04-27 08:22:59 +0200

    fmt

--- src/infrastructure/config_store.rs
+++ src/infrastructure/config_store.rs
@@ -108,10 +108,8 @@ impl CacheStore for FileConfigStore {
             #[cfg(unix)]
             {
                 use std::os::unix::fs::PermissionsExt;
-                let _ = std::fs::set_permissions(
-                    &cache_path,
-                    std::fs::Permissions::from_mode(0o600),
-                );
+                let _ =
+                    std::fs::set_permissions(&cache_path, std::fs::Permissions::from_mode(0o600));
             }
 
             Ok::<(), CacheError>(())
